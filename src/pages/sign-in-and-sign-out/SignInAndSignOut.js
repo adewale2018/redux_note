be877ff5df86381627a5a0ba99df9606a26cd796
Saheed Adewale Shittu <saheedadewaleshittu@gmail.com>
2020-10-18 15:15:45 +0100

    add signin, form-input and custom button

---
+++ src/pages/sign-in-and-sign-out/SignInAndSignOut.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./SignInAndSignOut.scss";
+import SignIn from "../../components/sign-in/SignIn";
+
+function SignInAndSignOut() {
+  return (
+    <div className='sign-in-and-sign-out'>
+      <SignIn />
+    </div>
+  );
+}
+
+export default SignInAndSignOut;
