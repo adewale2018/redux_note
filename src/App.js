c1df3f82553a13b391fc372a858b4d70d69460ff
Saheed Adewale Shittu <saheedadewaleshittu@gmail.com>
2020-10-18 22:25:43 +0100

    firebase user authetication

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import React from "react";
+import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
 
 import "./App.css";
 import HomePage from "./pages/homepage/HomePage";
 import ShopPage from "./pages/shop/ShopPage";
 import Header from "./components/header/Header";
-import SignInAndSignOut from './pages/sign-in-and-sign-out/SignInAndSignOut'
+import SignInAndSignOut from "./pages/sign-in-and-sign-out/SignInAndSignOut";
+import { auth } from "./firebase/firebase.utils";
 
-function App() {
-  return (
-    <>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
-        <Route path='/signin' component={SignInAndSignOut} />
-      </Switch>
-    </>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentUser: null,
+    };
+  }
+
+  onSubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.onSubscribeFromAuth = auth.onAuthStateChanged((user) => {
+      this.setState({ currentUser: user });
+      console.log("USER", user);
+    });
+  }
+
+  componentWillUnmount() {
+    this.onSubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <>
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInAndSignOut} />
+        </Switch>
+      </>
+    );
+  }
 }
 
 export default App;
