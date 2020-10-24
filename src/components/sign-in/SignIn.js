e4128c7fe1450af2ee72e4ff881fd106d2db67f7
Saheed Adewale Shittu <saheedadewaleshittu@gmail.com>
2020-10-24 15:19:17 +0100

    feat: add sign in with email and password logic

--- src/components/sign-in/SignIn.js
+++ src/components/sign-in/SignIn.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 import "./SignIn.scss";
 import FormInput from "../form-input/FormInput";
 import CustomButton from "../custom-button/CustomButton";
-import { SignInWithGoogle } from "../../firebase/firebase.utils";
+import { auth, SignInWithGoogle } from "../../firebase/firebase.utils";
 
 class SignIn extends Component {
   constructor(props) {
@@ -15,12 +15,19 @@ class SignIn extends Component {
     };
   }
 
-  handleSubmit = (e) => {
+  handleSubmit = async (e) => {
     e.preventDefault();
-    this.setState({
-      email: "",
-      password: "",
-    });
+const { email, password} = this.state;
+try {
+await auth.signInWithEmailAndPassword(email,password);
+this.setState({
+  email: "",
+  password: "",
+});
+}catch(error){
+
+}
+    
   };
 
   handleChange = (e) => {
